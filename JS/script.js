8c55fc2634592f9d071a4bb2b1ecfa02c9c87c95
Miguel Carmona <miguelcarmona996@gmail.com>
2022-05-12 11:47:16 -0400

    First commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+/*
+This will cause the navbar menu to open and close by
+clicking on the .hamburger-menu button
+*/
+
+const ham = document.querySelector('.header .navBar .hamburger-Menu');
+const nav = document.querySelector('.header .navBar nav ');
+ham.addEventListener('click', () => {
+    nav.classList.toggle('nav-toggle');
+    ham.classList.toggle('close');
+})
